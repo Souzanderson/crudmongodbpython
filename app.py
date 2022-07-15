66790902b440bc445ef3d22d65f8e284583ba66c
Anderson <andersonsouza.utfpr@gmail.com>
2022-07-15 15:44:55 -0300

    first

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from library.dbmongo import DbMongo
 
-db = DbMongo("mongodb+srv://anderson:159248@cluster0.7j0tz.mongodb.net/test","bancoteste", 27017)
+db = DbMongo("localhost","bancoteste", 27017)
 res = db.select("users")
 
 print(res)
